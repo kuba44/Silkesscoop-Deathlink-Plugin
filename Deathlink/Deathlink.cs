ebf87af896f241667d5750adf98c442eb8e6577a
Jakub Bulawkin <kubabulawkin@wp.pl>
2025-09-27 18:56:36 +0200

    Initial Commit

---
+++ Deathlink/Deathlink.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using SilklessLib;
+using System.Collections;
+using UnityEngine;
+
+[BepInPlugin("com.kuba44.Deathlink", "Deathlink for silklesscoop", "1.0.1")]
+[BepInDependency("SilklessLib")]
+public class Deathlink : BaseUnityPlugin
+{
+    private bool _canDie = true;
+    private float _deathCooldown = 10f;
+
+    private void Awake()
+    {
+        if (!SilklessAPI.Init())
+        {
+            Logger.LogError("Silkless plugin not found! Disabling Deathlink plugin.");
+            Destroy(this);
+        }
+        else
+        {
+            Logger.LogInfo("Deathlink plugin is loaded!");
+        }
+
+        SilklessAPI.AddHandler<DeathlinkPacket>(OnDeathlinkPacket);
+    }
+
+    private void Update()
+    {
+        if (HeroController.instance == null)
+            return;
+
+        if (_canDie && HeroController.instance.playerData.health == 0)
+        {
+            SendDeathlinkPacket();
+
+            StartCoroutine(DeathCooldown(_deathCooldown));
+        }
+    }
+
+    public class DeathlinkPacket : SilklessPacket
+    {
+
+    }
+
+    private void OnDeathlinkPacket(DeathlinkPacket packet)
+    {
+        HeroController.instance.DamageSelf(100);
+        LogUtil.LogInfo($"Player {packet.ID} has died", true);
+    }
+
+    private void SendDeathlinkPacket()
+    {
+        SilklessAPI.SendPacket(new DeathlinkPacket { });
+    }
+
+    IEnumerator DeathCooldown(float cooldown)
+    {
+        _canDie = false;
+
+        yield return new WaitForSeconds(cooldown);
+
+        _canDie = true;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Silkesscoop-Deathlink-Plugin
-A deathlink plugin for Silklesscoop by nek5
+# Silkesscoop Deathlink Plugin
+A deathlink plugin for [Silklesscoop by nek5](https://github.com/nek5s/SilklessCoop)
+
+Nexusmods page [here]()
+
+# Installation 
+* Download Silklesscoop from [nexusmods](https://www.nexusmods.com/hollowknightsilksong/mods/73) or [github](https://github.com/nek5s/SilklessCoop/releases) using the given instructions
+* Download Deathlink.zip from [nexusmods]() or [github](https://github.com/kuba44/Silkesscoop-Deathlink-Plugin/releases) and follow the same procedure as for Silklesscoop
+
+# Credits
+* [Silklesscoop made by nek5](https://github.com/nek5s/SilklessCoop)
